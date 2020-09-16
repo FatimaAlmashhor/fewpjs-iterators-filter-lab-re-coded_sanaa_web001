a3ecc5dbf5258965909cc587851eb235cf6a0a43
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-16 04:30:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,48 +48,48 @@ matchName( [
       ]
 , 'Bobby')
 
-  //   it('returns a driver if beginning provided letters match', function () {
-  //     expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
-  //   });
+    it('returns a driver if beginning provided letters match', function () {
+      expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
+    });
 
-  //   it('does not return drivers if only middle or ending letters match', function () {
-  //     expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
-  //   });
+    it('does not return drivers if only middle or ending letters match', function () {
+      expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
+    });
 
-  //   it('does not return drivers if only middle or ending letters match', function () {
-  //     expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
-  //   });
-  // });
+    it('does not return drivers if only middle or ending letters match', function () {
+      expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
+    });
+  });
 
-  // describe('matchName()', function () {
-  //   it('accesses the data structure to check if name matches', function () {
-  //     const drivers = [
-  //       {
-  //         name: 'Bobby',
-  //         hometown: 'Pittsburgh' },
-  //       {
-  //         name: 'Sammy',
-  //         hometown: 'New York' } ,
-  //       {
-  //         name: 'Sally',
-  //         hometown: 'Cleveland' },
-  //       {
-  //         name: 'Annette',
-  //         hometown: 'Los Angeles' },
-  //       {
-  //         name: 'Bobby',
-  //         hometown: 'Tampa Bay' }
-  //     ];
+  describe('matchName()', function () {
+    it('accesses the data structure to check if name matches', function () {
+      const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
 
-  //     expect(matchName(drivers, 'Bobby')).to.eql([
-  //       {
-  //         name: 'Bobby',
-  //         hometown: 'Pittsburgh'
-  //       },
-  //       {
-  //         name: 'Bobby',
-  //         hometown: 'Tampa Bay'
-  //       }
-  //     ]);
-  //   });
-  // });
+      expect(matchName(drivers, 'Bobby')).to.eql([
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh'
+        },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay'
+        }
+      ]);
+    });
+  });
