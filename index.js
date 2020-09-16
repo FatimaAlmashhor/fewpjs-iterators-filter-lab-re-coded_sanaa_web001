6784b6d1067d0383194bf509f99505ad35a84f5a
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-16 04:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Code your solution here
 function findMatching(arr  , name){
-   return arr.filter(e =>{ return (e === name)})
+   return arr.filter(e =>{ return (e === name)}) 
 }
   const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
     findMatching(drivers, 'Bobby');
     findMatching(drivers, 'Sammy');
-
-
+    
+   
    findMatching(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'], 'Bobby')
-
+    
     findMatching(drivers, 'Susan')
-
+    
     function fuzzyMatch(){
       const drivers = [];
         drivers.length = 0;
       drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
     }
-
+    
     fuzzyMatch(drivers, 'Sa');
     fuzzyMatch(drivers, 'y');
     fuzzyMatch(drivers, 'mm')
@@ -48,48 +48,48 @@ matchName( [
       ]
 , 'Bobby')
 
-    it('returns a driver if beginning provided letters match', function () {
-      expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
-    });
+  //   it('returns a driver if beginning provided letters match', function () {
+  //     expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
+  //   });
 
-    it('does not return drivers if only middle or ending letters match', function () {
-      expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
-    });
+  //   it('does not return drivers if only middle or ending letters match', function () {
+  //     expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
+  //   });
 
-    it('does not return drivers if only middle or ending letters match', function () {
-      expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
-    });
-  });
+  //   it('does not return drivers if only middle or ending letters match', function () {
+  //     expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
+  //   });
+  // });
 
-  describe('matchName()', function () {
-    it('accesses the data structure to check if name matches', function () {
-      const drivers = [
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh' },
-        {
-          name: 'Sammy',
-          hometown: 'New York' } ,
-        {
-          name: 'Sally',
-          hometown: 'Cleveland' },
-        {
-          name: 'Annette',
-          hometown: 'Los Angeles' },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay' }
-      ];
+  // describe('matchName()', function () {
+  //   it('accesses the data structure to check if name matches', function () {
+  //     const drivers = [
+  //       {
+  //         name: 'Bobby',
+  //         hometown: 'Pittsburgh' },
+  //       {
+  //         name: 'Sammy',
+  //         hometown: 'New York' } ,
+  //       {
+  //         name: 'Sally',
+  //         hometown: 'Cleveland' },
+  //       {
+  //         name: 'Annette',
+  //         hometown: 'Los Angeles' },
+  //       {
+  //         name: 'Bobby',
+  //         hometown: 'Tampa Bay' }
+  //     ];
 
-      expect(matchName(drivers, 'Bobby')).to.eql([
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh'
-        },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay'
-        }
-      ]);
-    });
-  });
+  //     expect(matchName(drivers, 'Bobby')).to.eql([
+  //       {
+  //         name: 'Bobby',
+  //         hometown: 'Pittsburgh'
+  //       },
+  //       {
+  //         name: 'Bobby',
+  //         hometown: 'Tampa Bay'
+  //       }
+  //     ]);
+  //   });
+  // });
